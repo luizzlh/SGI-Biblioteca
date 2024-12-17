56eaaa309db41a9cfb9d7ad63831e697a865b5d3
Luiz <luizzlh2004@gmail.com>
2024-12-17 20:31:08 -0300

    Implantado sistema de cadastro com mongoDB, inicio do sistema de login, métodos e classes para integração ao banco de dados e avanços no sistema de verificação.

---
+++ src/main/java/mongodb/InsereDadosUsuario.java
@@ -0,0 +1,25 @@
+package mongodb;
+import aplicattion.Pessoa;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+
+public class InsereDadosUsuario {
+    public static void insereDadosUsuario(Pessoa user) {
+        try (MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017")) {
+            MongoDatabase database = mongoClient.getDatabase("admin");
+            MongoCollection<Document> collection = database.getCollection("clientes");
+
+            Document doc = new Document("Nome", user.getNome())
+                    .append("Idade", user.getIdade())
+                    .append("Tipo", "Cliente")
+                    .append("ID", user.getId());
+            collection.insertOne(doc);
+
+            System.out.println("Documento inserido!");
+        }
+    }
+}
